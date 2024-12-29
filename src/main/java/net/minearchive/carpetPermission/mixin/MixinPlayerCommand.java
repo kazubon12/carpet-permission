b80d93617494dd4752b70d59f8dbf87c92f885ab
kazubon12 <103094882+kazubon12@users.noreply.github.com>
2024-12-30 03:44:20 +0900

    command block

--- src/main/java/net/minearchive/carpetPermission/mixin/MixinPlayerCommand.java
+++ src/main/java/net/minearchive/carpetPermission/mixin/MixinPlayerCommand.java
@@ -10,7 +10,6 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(PlayerCommand.class)
 public abstract class MixinPlayerCommand {
-
     @Redirect(method = "lambda$register$0", at = @At(value = "INVOKE", target = "Lcarpet/utils/CommandHelper;canUseCommand(Lnet/minecraft/commands/CommandSourceStack;Ljava/lang/Object;)Z"))
     private static boolean carpetPermission$checkPermission(CommandSourceStack source, Object commandLevel) {
         return check(source);
@@ -19,9 +18,9 @@ public abstract class MixinPlayerCommand {
     @Unique
     private static boolean check(CommandSourceStack source) {
         if (source.hasPermission(4)) return true;
+
         if (source.isPlayer()) {
             return CarpetPermissionServer.getInstance().getLevel(source.getPlayer().getUUID()) >= 1;
-        }
-        return false;
+        } else return true;
     }
 }
